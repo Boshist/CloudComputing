4b9e10ba6d5a69a936bb0be00d6b840b16125548
Boshist <31534147+Boshist@users.noreply.github.com>
2021-03-19 17:58:58 +0300

    pract1

---
+++ PureSumArray.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+
+int PureSumArray(int *Array, int start, int end);
+
+int main()
+{
+
+	int arr[] = {1, 2, 3, 4, 5, 6, 7, 8};
+	std::cout << PureSumArray(arr, 0, 8);
+	return 0;
+}
+
+int PureSumArray(int *Array, int start, int end) {
+
+	if (start < end)
+		return Array[start] + PureSumArray(Array, start + 1, end);
+	
+	return 0;
+}
